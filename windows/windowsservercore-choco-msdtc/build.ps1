7f8d2a561a0d64a222f7e785e0295cb6e9dab285
Oded Hanson <ohanson@xsightsys.com>
2017-07-16 17:02:54 +0300

    Fixed build scripts to use default docker network

--- windows/windowsservercore-choco-msdtc/build.ps1
+++ windows/windowsservercore-choco-msdtc/build.ps1
@@ -1,2 +1,8 @@
+$ErrorActionPreference = 'Stop'; # stop on all errors
+
+docker pull xsightsys/windowsservercore-chocolatey
+if ($LastExitCode -ne 0) {throw 'Command failed with exit code $LastExitCode.'};`
 docker build -t xsightsys/windowsservercore-choco-msdtc .
+if ($LastExitCode -ne 0) {throw 'Command failed with exit code $LastExitCode.'};`
 docker push xsightsys/windowsservercore-choco-msdtc:latest
+if ($LastExitCode -ne 0) {throw 'Command failed with exit code $LastExitCode.'};`
--- windows/windowsservercore-chocolatey/build.ps1
+++ windows/windowsservercore-chocolatey/build.ps1
@@ -1,2 +1,8 @@
+$ErrorActionPreference = 'Stop'; # stop on all errors
+
+docker pull microsoft/windowsservercore:latest
+if ($LastExitCode -ne 0) {throw 'Command failed with exit code $LastExitCode.'};`
 docker build -t xsightsys/windowsservercore-chocolatey .
+if ($LastExitCode -ne 0) {throw 'Command failed with exit code $LastExitCode.'};`
 docker push xsightsys/windowsservercore-chocolatey:latest
+if ($LastExitCode -ne 0) {throw 'Command failed with exit code $LastExitCode.'};`
